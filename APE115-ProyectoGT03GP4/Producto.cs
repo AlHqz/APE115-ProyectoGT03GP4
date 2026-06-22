0eab1e722dbff6513d9516e4b2914f7eaa031632
AlHqz <alejandro.bh03@gmail.com>
2026-06-21 18:14:02 -0600

    Feat: add DB connection, user views & remove accidentally tracked files

--- APE115-ProyectoGT03GP4/Producto.cs
+++ APE115-ProyectoGT03GP4/Producto.cs
@@ -1,10 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace APE115_ProyectoGT03GP4
 {
+    //Clase plantilla que representa un producto
     class Producto
     {
+        //Campos que componen el producto, según su estructura en la BDD
+        public int idProducto { get; set; }
+        public string descripcion { get; set; }
+        public decimal costo { get; set; }
+        public decimal precio { get; set; }
+        public char estado { get; set; }
+        public int stock { get; set; }
+        public int stockMinimo { get; set; }
+        public int idCategoria { get; set; }
+        //Constructor para la clase Producto con validaciones
+        public Producto (int idProducto, string descripcion, decimal costo, decimal precio, char estado, int stock, int stockMinimo, int idCategoria)
+        {
+            //Valida que los campos no sean inválidos
+            if (stockMinimo < 0 || stock < 0)
+                throw new ArgumentException("El stock y el stock mínimo no pueden ser negativos.");
+
+            if (precio <= 0 || costo < 0)
+                throw new ArgumentException("El precio/costo debe ser mayor a cero.");
+            //Asigna los valores a los campos de la clase
+            this.idProducto = idProducto;
+            this.descripcion = descripcion;
+            this.costo = costo;
+            this.precio = precio;
+            this.estado = estado;
+            this.stock = stock;
+            this.stockMinimo = stockMinimo;
+            this.idCategoria = idCategoria;
+        }
+        //Método para generar reportes de reabastecimiento
+        public bool Reabestecer()
+        {
+            return stock < stockMinimo;
+        }
     }
 }
---
+++ APE115-ProyectoGT03GP4/Vistas/Empleado/VistaEmpleado.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace APE115_ProyectoGT03GP4
+{
+    //Clase que controla la lógica del menú universal
+    public partial class VistaEmpleado : Window
+    {
+        //Definimos los atajos de teclado
+        public static RoutedCommand NavegarStockCmd = new RoutedCommand();
+        public static RoutedCommand NavegarGestionCmd = new RoutedCommand();
+        public static RoutedCommand NavegarInformeCmd = new RoutedCommand();
+        public VistaEmpleado()
+        {
+            InitializeComponent();
+
+            //Enlazamos los comandos a sus respectivas acciones
+            CommandBindings.Add(new CommandBinding(NavegarStockCmd, ExecutedNavegarStock));
+            CommandBindings.Add(new CommandBinding(NavegarGestionCmd, ExecutedNavegarGestion));
+            CommandBindings.Add(new CommandBinding(NavegarInformeCmd, ExecutedNavegarInforme));
+
+            //Configurar la combinación de teclas
+            InputBindings.Add(new KeyBinding(NavegarStockCmd, Key.Q, ModifierKeys.Control));
+            InputBindings.Add(new KeyBinding(NavegarGestionCmd, Key.G, ModifierKeys.Control));
+            InputBindings.Add(new KeyBinding(NavegarInformeCmd, Key.I, ModifierKeys.Control));
+        }
+        //Navegación con los atajos
+        private void ExecutedNavegarStock(object sender, ExecutedRoutedEventArgs e)
+        {
+            Navegar("/Vistas/Stock.xaml");
+        }
+
+        private void ExecutedNavegarInforme(object sender, ExecutedRoutedEventArgs e)
+        {
+            Navegar("/Vistas/Informe.xaml");
+        }
+
+        private void ExecutedNavegarGestion(object sender, ExecutedRoutedEventArgs e)
+        {
+            Navegar("/Vistas/Empleado/Gestion.xaml");
+        }
+        //Método reutilizable para navegar a cualquier vista
+        private void Navegar(string uri)
+        {
+            AreaContenido.Navigate(new Uri(uri, UriKind.Relative));
+        }
+    }
+}
